a05bf5244e137689bf5f4d6fde7791418bdbb309
Holger Brandl <holgerbrandl@gmail.com>
2016-03-16 21:55:21 +0100

    refined configuration model for parallelized collection

--- src/main/kotlin/kutils/ParCollections.kt
+++ src/main/kotlin/kutils/ParCollections.kt
@@ -6,17 +6,57 @@ import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
 /**
- * Parallel collection utils.
+ * Parallel collection mimicking  scala's .par().
  *
  * @author Holger Brandl
  */
 
+/** A delegated tagging interface to allow for parallized extension functions */
+class ParCol<T>(val it: Iterable<T>, val executorService: ExecutorService) : Iterable<T> by it
 
-/** A paralleizable version of kotlin.collections.map
+
+/** Convert a stream into a parallel collection. */
+fun <T> Iterable<T>.par(numThreads: Int = Runtime.getRuntime().availableProcessors() - 2,
+                        executorService: ExecutorService = Executors.newFixedThreadPool(numThreads)): ParCol<T> {
+    return ParCol(this, executorService);
+}
+
+
+/** De-palatalize a collection. Undos <code>par</code> */
+fun <T> ParCol<T>.unpar(): Iterable<T> {
+    return this.it;
+}
+
+
+fun <T, R> ParCol<T>.map(transform: (T) -> R): ParCol<R> {
+    // note default size is just an inlined version of kotlin.collections.collectionSizeOrDefault
+    val destination = ArrayList<R>(if (this is Collection<*>) this.size else 10)
+
+
+    for (item in this) {
+        executorService.submit { destination.add(transform(item)) }
+    }
+
+    //    executorService.shutdown()
+    executorService.awaitTermination(1, TimeUnit.DAYS)
+
+    return ParCol(destination, executorService)
+}
+
+
+/** A parallelizable version of kotlin.collections.map
  * @param numThreads The number of parallel threads. Defaults to number of cores minus 2
  * @param exec The executor. By exposing this as a parameter, application can share an executor among different parallel
  *             mapping taks.
  */
+fun <T, R> Iterable<T>.parmap(numThreads: Int = Runtime.getRuntime().availableProcessors() - 2,
+                              exec: ExecutorService = Executors.newFixedThreadPool(numThreads),
+                              transform: (T) -> R): Iterable<R> {
+    return this.par(executorService = exec).map(transform).unpar()
+}
+
+
+@Deprecated("use parmap() instead")
 fun <T, R> Iterable<T>.pmap(numThreads: Int = Runtime.getRuntime().availableProcessors() - 2, exec: ExecutorService = Executors.newFixedThreadPool(numThreads), transform: (T) -> R): List<R> {
     // note default size is just an inlined version of kotlin.collections.collectionSizeOrDefault
     val destination = ArrayList<R>(if (this is Collection<*>) this.size else 10)
@@ -30,3 +70,13 @@ fun <T, R> Iterable<T>.pmap(numThreads: Int = Runtime.getRuntime().availableProc
 
     return destination
 }
+
+
+internal fun main(args: Array<String>) {
+    val totalLength = listOf("test", "sdf", "foo").par().map { it.length }.fold(0, { sum, el -> sum + el })
+    println(totalLength)
+
+    //    listOf("test", "sdf", "foo").with {}
+}
+
+

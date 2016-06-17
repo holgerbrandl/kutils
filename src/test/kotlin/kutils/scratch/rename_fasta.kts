016da87ee8cb980d47dd410ef6571f7fb1b460b3
Holger Brandl <holgerbrandl@gmail.com>
2016-06-17 13:31:25 +0200

    cosmetics

--- src/test/kotlin/kutils/scratch/rename_fasta.kts
+++ src/test/kotlin/kutils/scratch/rename_fasta.kts
@@ -4,15 +4,17 @@ import kutils.bioinfo.openFasta
 import kutils.bioinfo.write
 import java.io.File
 
+
 val fastaFile = File(args[0])
-val outputFile = File(args[1])
+val assemblyName = args[1]
+val outputFile = File(assemblyName + ".fasta")
 
 // lazy approach but does not create a new file
 val sortedIds = openFasta(fastaFile).asSequence()
         .map { it.id to it.sequence.length }
         .sortedByDescending { it.second }
         .mapIndexed { index, id2length ->
-            id2length.first to "dd_Smes_G2_${index + 1}"
+            id2length.first to (assemblyName + "_{index + 1}")
         }.toMap()
 
 // rescan the fasta and replace IDs

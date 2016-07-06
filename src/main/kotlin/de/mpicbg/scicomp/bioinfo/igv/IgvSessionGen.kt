b734b42467af5c1ab3fbbf404721e9673b4aca54
Holger Brandl <holgerbrandl@gmail.com>
2016-07-06 10:54:56 +0200

    moved igv generator to kutils

---
+++ src/main/kotlin/de/mpicbg/scicomp/bioinfo/igv/IgvSessionGen.kt
@@ -0,0 +1,102 @@
+package de.mpicbg.scicomp.bioinfo.igv
+
+
+/**
+ * Support API to programmatically create an IGV session file.
+ */
+
+
+import java.io.File
+
+// todo optionally call igvtools to build appropriate indicies
+// todo move to dependency library for clarity
+// todo support genome file as input instead of fasta (see http://www.broadinstitute.org/igv/LoadGenome)
+// todo validate that session can be loaded at the end using programmatic interface of iGV
+
+// todo add option to build region set from existing bedfile to stream-line region navigation (or simply import bed file directly within IGV)
+
+interface IGVTrack {
+    fun toTrackXML(): String
+    fun getResourceFile(): File
+}
+
+class BamTrack(val bamFile: File) : IGVTrack {
+
+    constructor(bamFile: String) : this(File(bamFile))
+
+    override fun getResourceFile(): File = bamFile
+
+    override fun toTrackXML() = """        <Track altColor="0,0,178" autoScale="false" color="0,0,178" displayMode="COLLAPSED" featureVisibilityWindow="-1" fontSize="10" id="${bamFile.absolutePath}" name="${bamFile.name.removeSuffix(".bam")}" sortable="true" visible="true">
+            <RenderOptions colorByTag="" colorOption="READ_STRAND" flagUnmappedPairs="false" groupByTag="" maxInsertSize="1000" minInsertSize="50" shadeBasesOption="QUALITY" shadeCenters="true" showAllBases="false" sortByTag=""/>
+        </Track>"""
+}
+
+
+class VcfTrack(val vcfFile: File) : IGVTrack {
+    constructor(vcfFile: String) : this(File(vcfFile))
+
+    override fun getResourceFile(): File = vcfFile
+
+    override fun toTrackXML(): String = """        <Track SQUISHED_ROW_HEIGHT="4" altColor="0,0,178" autoScale="false" clazz="org.broad.igv.track.FeatureTrack" color="0,0,178" colorMode="GENOTYPE" displayMode="EXPANDED" featureVisibilityWindow="1998000" fontSize="10" id="${vcfFile.absolutePath}" name="${vcfFile.name.removeSuffix(".gz").removeSuffix("vcf")}" renderer="BASIC_FEATURE" sortable="false" visible="true" windowFunction="count"/>"""
+}
+
+class BedTrack(val bedFile: File) : IGVTrack {
+    constructor(bedFile: String) : this(File(bedFile))
+
+    override fun getResourceFile(): File = bedFile
+
+    override fun toTrackXML(): String = """        <Track altColor="0,0,178" autoScale="false" clazz="org.broad.igv.track.FeatureTrack" color="0,0,178" displayMode="COLLAPSED" featureVisibilityWindow="1000000" fontSize="10" id="${bedFile.absolutePath}" name="${bedFile.name.removeSuffix(".gz").removeSuffix(".bed")}" renderer="BASIC_FEATURE" sortable="false" visible="true" windowFunction="count"/>
+"""
+}
+
+class BigWigTrack(val bwFile: File) : IGVTrack {
+    constructor(bwFile: String) : this(File(bwFile))
+
+    override fun getResourceFile(): File = bwFile
+
+    override fun toTrackXML(): String = """                <Track altColor="0,0,178" autoScale="true" clazz="org.broad.igv.track.DataSourceTrack" color="0,0,178" displayMode="COLLAPSED" featureVisibilityWindow="-1" fontSize="10" id="${bwFile.absolutePath}" name="${bwFile.name.removeSuffix(".bw")}" normalize="false" renderer="BAR_CHART" sortable="true" visible="true" windowFunction="mean">
+            <DataRange baseline="0.0" drawBaseline="true" flipAxis="false" maximum="960.5058" minimum="0.0" type="LINEAR"/>
+        </Track>
+
+    """
+}
+
+fun builSession(genome: String, tracks: List<IGVTrack>): String {
+
+    val resourceXML = tracks.map { it.getResourceFile() }.map { """<Resource path="${it}"/>""" }.joinToString("\n")
+    val trackXML = tracks.map { it.toTrackXML() }.joinToString("\n")
+
+    return """
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<Session genome="${if (File(genome).exists()) File(genome).absolutePath else genome}" hasGeneTrack="false" hasSequenceTrack="true" path="/Volumes/RADseq-planarian/radseq/schMedP1_mapping/igv_session_ratio_difference.xml" version="8">
+    <Resources>
+       ${resourceXML}
+    </Resources>
+    <Panel height="962" name="DataPanel" width="2504">
+        ${trackXML}
+    </Panel>
+    <Panel height="137" name="FeaturePanel" width="2504">
+        <Track altColor="0,0,178" autoScale="false" color="0,0,178" displayMode="COLLAPSED" featureVisibilityWindow="-1" fontSize="10" id="Reference sequence" name="Reference sequence" sortable="false" visible="true"/>
+    </Panel>
+    <PanelLayout dividerFractions="0.8716094032549728"/>
+    <HiddenAttributes>
+        <Attribute name="DATA FILE"/>
+        <Attribute name="DATA TYPE"/>
+        <Attribute name="NAME"/>
+    </HiddenAttributes>
+</Session>
+""".trim()
+
+}
+
+fun guessTracks(vararg trackFiles: String) = guessTracks(*trackFiles.map { File(it) }.toTypedArray())
+
+fun guessTracks(vararg trackFiles: File) = trackFiles.map {
+    when {
+        it.name.endsWith(".bed") -> BedTrack(it)
+        it.name.endsWith(".bam") -> BamTrack(it)
+        it.name.endsWith(".vcf") -> VcfTrack(it)
+        it.name.endsWith(".bw") -> VcfTrack(it)
+        else -> throw IllegalArgumentException("unsupported track type")
+    }
+}
---
+++ src/main/kotlin/de/mpicbg/scicomp/bioinfo/igv/make_igv_session.kts
@@ -0,0 +1,26 @@
+#!/usr/bin/env kscript
+
+package de.mpicbg.scicomp.bioinfo.igv
+
+
+/**
+ * Programmatically create an IGV session file.
+ */
+
+
+// cd /Volumes/RADseq-planarian/radseq/dd_Smes_g2_mapping; export PATH=/Users/brandl/IdeaProjects/kotlin_playground/src:${PATH}
+// chmod +x make_igv_session.kts
+// make_igv_session.kts
+
+// configure properties of igv session
+val genome = "/Volumes/RADseq-planarian/radseq/dd_Smes_g2_mapping/genome/dd_Smes_g2.fasta" // can also be valid identifier like mm10
+
+val tracks = listOf(BamTrack("SP37.bam"), BamTrack("SP90.bam"), BedTrack("uniq_var_sites_SP37.bed"), BedTrack("uniq_var_sites_SP90.bed"), BigWigTrack("SP37.bw"), BigWigTrack("SP90.bw"))
+
+// rather print to stdut to allow easy post-processing
+builSession(genome, tracks).let { println(it) }
+
+
+// test run
+// /Users/brandl/IdeaProjects/kotlin_playground/src/make_igv_session.kts/make_igv_session.kts
+

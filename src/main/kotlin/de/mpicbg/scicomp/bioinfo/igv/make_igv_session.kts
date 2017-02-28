66f7a63bc23feb5d2e9852ca3efff7f652545193
Holger Brandl <holgerbrandl@gmail.com>
2017-02-28 17:47:59 +0100

    added more validity checks for argument track files

--- src/main/kotlin/de/mpicbg/scicomp/bioinfo/igv/make_igv_session.kts
+++ src/main/kotlin/de/mpicbg/scicomp/bioinfo/igv/make_igv_session.kts
@@ -21,11 +21,39 @@ if (args.isEmpty()) {
 
 
 val genome = args.first()
-val tracks = guessTracks(args.drop(1).map { File(it) })
+val trackFiles = args.drop(1).map { File(it) }
+
+// validate files
+if (trackFiles.any { !it.exists() }) {
+    val missingTrackFiles = trackFiles.filter { !it.exists() }
+    System.err.println("Error: Some track files were missing:\n${missingTrackFiles.joinToString { "\n" }}")
+    exitProcess(1)
+}
+
+
+// TODO Do more input validation
+// * sorsted beds
+// * indexed bams
+val missingIndexBams = trackFiles
+        .filter { it.name.endsWith(".bam") }
+        .filter { !File(it.absolutePath + ".bai").exists() }
+
+if (!missingIndexBams.isEmpty()) {
+    System.err.println("Error: Bam indices are missing for the following files but are required by IGV:\n" +
+            missingIndexBams.joinToString("\n"))
+    exitProcess(1)
+}
+
+
+// guess track types
+val tracks = guessTracks(trackFiles)
 
 // rather print to stdut to allow easy post-processing
 builSession(genome, tracks).let { println(it) }
 
 
 // /Users/brandl/IdeaProjects/kotlin_playground/src/make_igv_session.kts/make_igv_session.kts
+
+
+
 

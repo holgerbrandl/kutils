6e65439a53768e60c089c1fe29cf2c480ca7f44e
Holger Brandl <holgerbrandl@gmail.com>
2018-09-05 21:45:12 +0200

    Improved benchmarking utility

--- src/main/kotlin/de/mpicbg/scicomp/kutils/MicroBenchmark.kt
+++ src/main/kotlin/de/mpicbg/scicomp/kutils/MicroBenchmark.kt
@@ -4,10 +4,8 @@ package de.mpicbg.scicomp.kutils
  * @author Holger Brandl
  */
 
-class MicroBenchmark<T> {
+class MicroBenchmark<T>(var reps:Int = 25, var warmupReps:Int = 10) {
 
-    var reps = 25
-    var warmupReps = 10
 
     val results = emptyList<BenchmarkResult<T>>().toMutableList()
 
@@ -39,10 +37,22 @@ internal fun List<Number>.mean(): Double = map { it.toDouble() }.sum() / size
 
 internal fun List<Number>.sd() = if (size == 1) null else Math.sqrt(map { Math.pow(it.toDouble() - mean(), 2.toDouble()) }.sum() / size.toDouble())
 
+
 data class BenchmarkResult<T>(val times: List<Long>, val config: T?) {
     fun printSummary() {
-        println("${config}:\t  mean=${times.mean() / 1E9}\t  sd=${times.sd() ?: kotlin.Double.Companion.NaN/1E9}")
+        println(getSummary())
     }
+
+    fun getSummary() = "${config}:\t  mean=${mean}\t  sd=${sd}"
+
+    val timesMS:List<Double>
+        get() = times.map { it.toDouble()/1E6 }
+
+    val mean: Double
+        get() = timesMS.mean()
+
+    val sd: Double
+        get() = timesMS.sd() ?: Double.NaN
 }
 
 fun main(args: Array<String>) {
